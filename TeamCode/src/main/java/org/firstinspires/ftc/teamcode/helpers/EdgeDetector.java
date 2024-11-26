d4bba8ee4f5ae59586947ca1c23369dc715755ef
lucaci32u4 <lucaci32u4@gmail.com>
2024-11-27 01:41:21 +0200

    Add simple edge detector

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/EdgeDetector.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.helpers;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EdgeDetector {
+
+    private final List<Runnable> onPress = new ArrayList<>();
+    private final List<Runnable> onRelease = new ArrayList<>();
+
+    private boolean state;
+
+    public EdgeDetector(boolean initialState) {
+        this.state = initialState;
+    }
+
+    void update(boolean newState) {
+
+        // Update the state variable early on
+        // To make calls to getter return the correct variable
+        // When calling from a lambda function
+        boolean oldState = state;
+        state = newState;
+
+        // Notify event handlers
+        if (newState && !oldState) {
+            onPress.forEach(Runnable::run);
+        }
+        if (!newState && oldState) {
+            onRelease.forEach(Runnable::run);
+        }
+
+    }
+
+    void onPress(Runnable action) {
+        onPress.add(action);
+    }
+
+    void onRelease(Runnable action) {
+        onRelease.add(action);
+    }
+
+    public boolean state() {
+        return state;
+    }
+
+}
